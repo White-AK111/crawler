e3c44aba3ff7492c881be925d20a383c263411a4
White-AK111 <formatmyc@gmail.com>
2021-12-03 02:53:14 -0800

    fix pre-commit config

--- crawler/crawler_test.go
+++ crawler/crawler_test.go
@@ -136,7 +136,7 @@ func TestGet(t *testing.T) {
 		}
 	})
 
-	addr := "localhost:8080"
+	addr := "localhost:8081"
 	url := "http://" + addr + "/home/"
 	server := &http.Server{Addr: addr, Handler: nil}
 	go func() {
